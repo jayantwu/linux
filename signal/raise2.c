9b7da4a91d8657ee002819fdcaacf48f7bc0c0c5
jayantwu <31072285+jayantwu@users.noreply.github.com>
2021-02-24 22:16:42 +0800

    Update raise2.c

--- signal/raise2.c
+++ signal/raise2.c
@@ -11,9 +11,9 @@ int main(int argc, char** argv)
   if (pid > 0)
   {
     sleep(8);
-    if (waitpid(pid, NULL, WNOHANG)==0)
+    if (waitpid(pid, NULL, WNOHANG)==0)  //非阻塞方式     父进程返回的是子进程的pid
     {
-      kill(pid, 9);
+      kill(pid, 9);   //向子进程发送kill信号， 子进程结束之后，父进程调用wait()函数回收资源，否则子进程成为僵尸进程
     }
     wait(NULL);
     while(1);
@@ -22,7 +22,7 @@ int main(int argc, char** argv)
   if (pid == 0)
   {
     printf("raise function before\n");
-    raise(SIGTSTP);
+    raise(SIGTSTP);  //暂停
     printf("raise fuunction after\n");
     exit(0);
   }
