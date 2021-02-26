04222c458598c1a2561208e6bf3883667973cf80
wujiayang <1064216585@qq.com>
2021-02-27 00:11:56 +0800

    readline/recv

---
+++ socket/echocli_line.cpp
@@ -0,0 +1,166 @@
+﻿#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h> 
+
+
+
+ssize_t readn(int fd, void *buf, size_t count)
+{
+	size_t nleft = count;  //剩余需要读的字节数 
+	ssize_t nread;   //每次读的字节数 
+	char * bufp = (char*)buf;
+	while(nleft)//非0时一直循环读 
+	{
+		if((nread = read(fd, bufp, nleft))<0)
+		{
+			if (errno == EINTR)
+				continue;
+			return -1;
+		}
+		else if(nread == 0)
+		{
+			return count - nleft; //返回读了多少 
+		}
+		bufp += nread;
+		nleft -= nread;
+	}
+	return count;
+}
+ssize_t writen(int fd, const void *buf, size_t count)
+{
+	size_t nleft = count;
+	ssize_t nwrite;
+	char * bufp = (char*)buf;
+	while(nleft)
+	{
+		if((nwrite = write(fd, bufp, nleft))<0)
+		{
+			if (errno == EINTR)
+				continue;
+			return -1;
+		}
+		else if(nwrite == 0)
+		{
+			continue;
+		}
+		bufp += nwrite;
+		nleft -= nwrite;
+	}
+	return count;
+}
+
+//偷窥的方式读取 
+ssize_t recv_peek(int sockfd, void *buf, size_t len)
+{
+	while(1)
+	{
+		int ret = recv(sockfd, buf, len, MSG_PEEK);
+		if (ret == -1 && errno == EINTR)  //返回失败或异常中断 
+			continue;
+		return ret;
+	}
+}
+//可处理粘包  读一行，遇到'\n'才返回 
+ssize_t readline(int sockfd, void *buf, size_t maxline)
+{
+	int ret;
+	int nread; //已经读的字节数 
+	char *bufp = (char*)buf;
+	int nleft = maxline;
+	while(1)
+	{
+		ret = recv_peek(sockfd, bufp, nleft);
+		if (ret < 0)
+			return ret;
+		else if (ret == 0)
+			return ret;
+		nread = ret;
+		int i;
+		for (i = 0; i < nread; i++)
+		{
+			if (bufp[i] == '\n')
+			{
+				ret = readn(sockfd, bufp, i+1);//读走，包括'\n'
+				if (ret != i+1)
+					exit(EXIT_SUCCESS);
+				return ret; 
+			}
+		}
+		//没有遇到'\n' ，需要继续读 
+		if (nread > nleft)//处理异常 
+			exit(EXIT_SUCCESS);
+		nleft -= nread;
+		ret = readn(sockfd, bufp, nread);//全部读到buff里 
+		if (ret != nread)
+			exit(EXIT_SUCCESS);
+		bufp += nread;  //指针更新 
+	}
+	return -1;
+}
+
+
+int  main(int argc, char **argv)
+{
+	if (argc < 3)
+	{
+		printf("use client 127.0.0.1 5188\n");
+		return -1;
+	}
+	
+	
+	int sock;
+	short int port = atoi(argv[2]);
+	if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP))< 0)
+	/*if ((listenfd = socket(PF_INET, SOCK_STREAM, 0))< 0)*/  //第三个参数可以为0，前面两个参数实际已经决定是tcp了
+	{
+		perror("socket fail\n");
+		return -1;
+	}
+	
+	struct sockaddr_in servaddr;
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(port);
+
+	//servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+	if(connect(sock, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
+	{
+		perror("connect fail\n");
+		return -1;
+	}
+	char sendbuf[1024] = {0};
+	char recvbuf[1024] = {0};
+	memset(sendbuf, 0, sizeof(sendbuf));
+	memset(recvbuf, 0, sizeof(recvbuf));
+	while (fgets(sendbuf, sizeof(sendbuf), stdin) != NULL)
+	{
+		
+		writen(sock, sendbuf, strlen(sendbuf));  //发送 
+		
+		int ret = readline(sock, recvbuf, sizeof(recvbuf)); 
+	
+		if (ret == -1)  //读失败 
+			perror("readline fail\n");
+
+		if (ret == 0)
+		{
+			printf("server close\n");
+			break;
+		} 
+		
+		printf("recv from server: ");
+		fputs(recvbuf, stdout);
+		memset(sendbuf, 0, sizeof(sendbuf));//要清掉，否则可能有上一次的垃圾值 
+		memset(recvbuf, 0, sizeof(recvbuf));
+	} 
+	//关闭套接字 
+	close(sock);
+	
+	
+}
---
+++ socket/echosrv_line.cpp
@@ -0,0 +1,221 @@
+﻿#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h> 
+#include<signal.h>
+
+
+ssize_t readn(int fd, void *buf, size_t count)
+{
+	size_t nleft = count;
+	ssize_t nread;
+	char * bufp = (char*)buf;
+	while(nleft)
+	{
+		if((nread = read(fd, bufp, nleft))<0)
+		{
+			if (errno == EINTR)
+				continue;
+			return -1;
+		}
+		else if(nread == 0)
+		{
+			return count - nleft;
+		}
+		bufp += nread;
+		nleft -= nread;
+	}
+	return count;
+}
+ssize_t writen(int fd, const void *buf, size_t count)
+{
+	size_t nleft = count;
+	ssize_t nwrite;
+	char * bufp = (char*)buf;
+	while(nleft)
+	{
+		if((nwrite = write(fd, bufp, nleft))<0)
+		{
+			if (errno == EINTR)
+				continue;
+			return -1;
+		}
+		else if(nwrite == 0)
+		{
+			continue;
+		}
+		bufp += nwrite;
+		nleft -= nwrite;
+	}
+	return count;
+}
+
+ssize_t recv_peek(int sockfd, void *buf, size_t len)
+{
+	while(1)
+	{
+		int ret = recv(sockfd, buf, len, MSG_PEEK);
+		if (ret == -1 && errno == EINTR)
+			continue;
+		return ret;
+	}
+}
+
+ssize_t readline(int sockfd, void *buf, size_t maxline)
+{
+	int ret;
+	int nread; 
+	char *bufp = (char*)buf;
+	int nleft = maxline;
+	while(1)
+	{
+		ret = recv_peek(sockfd, bufp, nleft);
+		if (ret < 0)
+			return ret;
+		else if (ret == 0)
+			return ret;
+		nread = ret;
+		int i;
+		for (i = 0; i < nread; i++)
+		{
+			if (bufp[i] == '\n')
+			{
+				ret = readn(sockfd, bufp, i+1);//读走，包括'\n'
+				if (ret != i+1)
+					exit(EXIT_SUCCESS);
+				return ret; 
+			}
+		}
+		if (nread > nleft)
+			exit(EXIT_SUCCESS);
+		nleft -= nread;
+		ret = readn(sockfd, bufp, nread);
+		if (ret != nread)
+			exit(EXIT_SUCCESS);
+		bufp += nread;
+	}
+	return -1;
+}
+
+
+void do_service(int conn)
+{
+	char recvbuf[1024];
+	int n;
+	while (1)
+	{
+		memset(&recvbuf, 0, sizeof(recvbuf));
+		int ret = readline(conn, recvbuf, 1024);
+		if (ret == -1)
+			perror("readline fail\n");
+		if(ret ==0)
+		{
+			printf("client close\n");
+		}
+        
+		printf("recv from client:%s", recvbuf);
+		writen(conn, recvbuf, strlen(recvbuf));
+	} 
+	return;
+}
+
+
+int  main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		printf("use \"server port\"\n");
+		return -1;
+	}
+	
+	int listenfd;
+	short int port = atoi(argv[1]);
+	if ((listenfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP))< 0)
+	/*if ((listenfd = socket(PF_INET, SOCK_STREAM, 0))< 0)*/  //第三个参数可以为0，前面两个参数实际已经决定是tcp了
+	{
+		perror("socket fail\n");
+		return -1;
+	}
+	
+	struct sockaddr_in servaddr;
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(port);
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	/*
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");  
+	或者： 
+	//int inet_aton(const char *cp, struct in_addr *inp);
+	将地址转为网络序 
+	inet_aton("127.0.0.1", (struct sockaddr*)&servaddr.sin_addr) 
+	*/ 
+	
+	int on =1;
+	if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0)
+	{
+		perror("setsockopt fail\n");
+		return -1;
+	}
+	
+	//绑定fd和ip端口 
+	if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
+	{
+		perror("bind fail\n");
+		return -1;
+	} 
+	//listenfd为被动套接字，能够接受连接 
+	if (listen(listenfd, SOMAXCONN) < 0)  //已完成连接和未完成连接队列的和 队列的最大值 
+	{
+		perror("listen fail\n");
+		return -1;
+	} 
+	
+	struct sockaddr_in peeraddr;
+	socklen_t peerlen = sizeof(peeraddr);   //peerlen一定要初始化 
+	int conn;  //accept 返回一个连接 ，新的套接字，主动套接字，可以用来发送请求 
+	
+	pid_t pid;
+	while(1)
+	{   
+	    //signal(SIGUSR1, handler);
+		//连接socket 用于通信 
+		if((conn = accept(listenfd, (struct sockaddr*)&peeraddr, &peerlen))<0)
+	    {
+		    perror("accept fail\n");
+		    return -1;
+	    }
+	    printf("client ip :%s; client port: %d\n", inet_ntoa(peeraddr.sin_addr),
+	       ntohs(peeraddr.sin_port));
+	       
+        pid = fork();
+        if (pid == -1)
+        {
+        	perror("fork fail\n");
+        	return -1;
+        }
+        
+        if (pid == 0)  //子进程
+		{
+			//cnt++;
+			//printf("client + 1\n");
+			close(listenfd);
+			do_service(conn);
+			//kill(getppid(), SIGUSR1); 
+			exit(0);
+		} 
+		if (pid > 0)
+		{
+			
+			close(conn);//父进程不需要连接socket 
+		}
+	} 
+	
+	
+	return 0;
+	
+	
+}
