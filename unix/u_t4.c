240337ca174fd8b18fe83f4bd32bc42b0ae7c932
“wujiayang” <1064216585@qq.com>
2023-09-14 00:01:31 +0800

    test linux read/write

--- unix/u_t4.c
+++ unix/u_t4.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 
 int main()
 {
@@ -18,6 +19,21 @@ int main()
     cur_pos = lseek(fd, 2000, SEEK_SET);
     write(fd, buf, sizeof(buf));
 
+    int fd2 = open("./file2.txt", O_RDWR);
+    
+    char buffer[1024];
+    int n = 0;
+    memset(buffer, 0, sizeof(buffer));
+    while( (n = read(fd2, buffer, 16)) > 0 ) {
+        write(STDOUT_FILENO, buffer, n);
+    }
+    printf("\n");
+    lseek(fd2, 0, SEEK_SET); // 将指针重新指向文件开始
+    n = read(fd2, buffer, 16);  // 读16个字节
+    write(STDOUT_FILENO, buffer, n); // 向标准输出写16个字节
+    printf("\n");
+
+    close(fd2);
     close(fd);
     return 0;
 }
